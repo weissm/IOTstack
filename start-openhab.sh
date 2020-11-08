72a377455516cde31e468f6af2fb7c3779075a79
weissm <matthias.weiss@posteo.de>
2020-11-08 09:02:32 +0100

    added recent updates

--- start-openhab.sh
+++ start-openhab.sh
@@ -1,1 +1,17 @@
-docker run --name openhab --net=host --tty -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro -v openhab_addons:/openhab/addons -v openhab_conf:/openhab/conf -v openhab_userdata:/openhab/userdata -d --restart=always openhab/openhab:latest
+docker run --name openhab \
+--net=host \
+--tty \
+-v /etc/localtime:/etc/localtime:ro \
+-v /etc/timezone:/etc/timezone:ro \
+-v /home/pi/shared/IOTstack/volumes/openhab/addons:/openhab/addons:rw \
+-v /home/pi/shared/myscripts/openhab/conf:/openhab/conf:rw \
+-v /home/pi/shared/IOTstack/volumes/openhab/userdata:/openhab/userdata:rw \
+-d \
+-e OPENHAB_HTTP_PORT=4050 \
+-e OPENHAB_HTTPS_PORT=4051 \
+-e USER_ID=1000 \
+-e GROUP_ID=115 \
+-e "EXTRA_JAVA_OPTS=-Duser.timezone=Europe/Berlin" \
+--restart=always \
+openhab/openhab:latest
+
